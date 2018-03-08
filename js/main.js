4b2a8bbf6fc68f71385b26ee852446a6fa204650
ihorba <ihor@footballaddicts.com>
2018-03-08 20:10:25 +0200

    footer

--- js/main.js
+++ js/main.js
@@ -6,6 +6,7 @@
 		teamSlider.slick({
 			slide: '.ba-portfolio__slide',
 			arrows: true,
+			// centerPadding: '0px',
 			prevArrow: '.ba-portfolio-prev',
 			nextArrow: '.ba-portfolio-next',
 			slidesToShow: 3,
@@ -13,11 +14,10 @@
 			focusOnSelect: true,
 			responsive: [
 			{
-				breakpoint: 768,
+				breakpoint: 480,
 				settings: {
 					centerMode: true,
 					slidesToShow: 1,
-					variableWidth: false,
 				}
 			}
 			]
@@ -38,6 +38,7 @@
 			arrows: false,
 		 	infinite: true,
 			centerMode: true,
+			centerPadding: '0px',
 			slidesToShow: 3,
 			slidesToScroll: 1,
 			focusOnSelect: true,
@@ -120,181 +121,5 @@
 	  });
 	  });
 
-
-
-	  // function initMap() {
-	  // 	var roma = {lat: 42.462473, lng: 11.832275};
-
-	  // 	var mapDiv = document.getElementById('ba-map');
-
-	  // 	var map = new google.maps.Map(document.getElementById('ba-map'), {
-	  // 		zoom: 6,
-	  // 		center: roma,
-	  // 		disableDefaultUI: true,
-	  // 		styles: [
-	  // 		{
-	  // 			"elementType": "geometry",
-	  // 			"stylers": [
-	  // 			{
-	  // 				"color": "#f5f5f5"
-	  // 			}
-	  // 			]
-	  // 		},
-	  // 		{
-	  // 			"elementType": "labels.icon",
-	  // 			"stylers": [
-	  // 			{
-	  // 				"visibility": "off"
-	  // 			}
-	  // 			]
-	  // 		},
-	  // 		{
-	  // 			"elementType": "labels.text.fill",
-	  // 			"stylers": [
-	  // 			{
-	  // 				"color": "#616161"
-	  // 			}
-	  // 			]
-	  // 		},
-	  // 		{
-	  // 			"elementType": "labels.text.stroke",
-	  // 			"stylers": [
-	  // 			{
-	  // 				"color": "#f5f5f5"
-	  // 			}
-	  // 			]
-	  // 		},
-	  // 		{
-	  // 			"featureType": "administrative.land_parcel",
-	  // 			"elementType": "labels.text.fill",
-	  // 			"stylers": [
-	  // 			{
-	  // 				"color": "#bdbdbd"
-	  // 			}
-	  // 			]
-	  // 		},
-	  // 		{
-	  // 			"featureType": "poi",
-	  // 			"elementType": "geometry",
-	  // 			"stylers": [
-	  // 			{
-	  // 				"color": "#eeeeee"
-	  // 			}
-	  // 			]
-	  // 		},
-	  // 		{
-	  // 			"featureType": "poi",
-	  // 			"elementType": "labels.text.fill",
-	  // 			"stylers": [
-	  // 			{
-	  // 				"color": "#757575"
-	  // 			}
-	  // 			]
-	  // 		},
-	  // 		{
-	  // 			"featureType": "poi.park",
-	  // 			"elementType": "geometry",
-	  // 			"stylers": [
-	  // 			{
-	  // 				"color": "#e5e5e5"
-	  // 			}
-	  // 			]
-	  // 		},
-	  // 		{
-	  // 			"featureType": "poi.park",
-	  // 			"elementType": "labels.text.fill",
-	  // 			"stylers": [
-	  // 			{
-	  // 				"color": "#9e9e9e"
-	  // 			}
-	  // 			]
-	  // 		},
-	  // 		{
-	  // 			"featureType": "road",
-	  // 			"elementType": "geometry",
-	  // 			"stylers": [
-	  // 			{
-	  // 				"color": "#ffffff"
-	  // 			}
-	  // 			]
-	  // 		},
-	  // 		{
-	  // 			"featureType": "road.arterial",
-	  // 			"elementType": "labels.text.fill",
-	  // 			"stylers": [
-	  // 			{
-	  // 				"color": "#757575"
-	  // 			}
-	  // 			]
-	  // 		},
-	  // 		{
-	  // 			"featureType": "road.highway",
-	  // 			"elementType": "geometry",
-	  // 			"stylers": [
-	  // 			{
-	  // 				"color": "#dadada"
-	  // 			}
-	  // 			]
-	  // 		},
-	  // 		{
-	  // 			"featureType": "road.highway",
-	  // 			"elementType": "labels.text.fill",
-	  // 			"stylers": [
-	  // 			{
-	  // 				"color": "#616161"
-	  // 			}
-	  // 			]
-	  // 		},
-	  // 		{
-	  // 			"featureType": "road.local",
-	  // 			"elementType": "labels.text.fill",
-	  // 			"stylers": [
-	  // 			{
-	  // 				"color": "#9e9e9e"
-	  // 			}
-	  // 			]
-	  // 		},
-	  // 		{
-	  // 			"featureType": "transit.line",
-	  // 			"elementType": "geometry",
-	  // 			"stylers": [
-	  // 			{
-	  // 				"color": "#e5e5e5"
-	  // 			}
-	  // 			]
-	  // 		},
-	  // 		{
-	  // 			"featureType": "transit.station",
-	  // 			"elementType": "geometry",
-	  // 			"stylers": [
-	  // 			{
-	  // 				"color": "#eeeeee"
-	  // 			}
-	  // 			]
-	  // 		},
-	  // 		{
-	  // 			"featureType": "water",
-	  // 			"elementType": "geometry",
-	  // 			"stylers": [
-	  // 			{
-	  // 				"color": "#c9c9c9"
-	  // 			}
-	  // 			]
-	  // 		},
-	  // 		{
-	  // 			"featureType": "water",
-	  // 			"elementType": "labels.text.fill",
-	  // 			"stylers": [
-	  // 			{
-	  // 				"color": "#9e9e9e"
-	  // 			}
-	  // 			]
-	  // 		}
-	  // 		]
-	  // 	});
-
-	  // }
-
-	  // window.onload = initMap;
 
 	})(jQuery);
